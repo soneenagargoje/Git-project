940ff8d4ea8f2287453e2a400fc95ce7c4a24eb6
soneepnagargoje <soneenagargoje480@gmail.com>
2023-03-17 11:42:53 +0530

    Add files via upload

---
+++ March17.java
@@ -0,0 +1,17 @@
+package com.march17;
+
+public class March17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=5;j>=i;j--)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();		}
+	}
+
+}
