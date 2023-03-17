3f8ab0853e413c2cedf5a0561a03efd97d4bbf25
soneepnagargoje <soneenagargoje480@gmail.com>
2023-03-17 12:00:14 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package om.march17;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=34;
+		int b=43;
+		System.out.println("multiplication is ="+(a*b));
+	}
+
+}
